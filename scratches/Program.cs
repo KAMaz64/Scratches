890adf691f370aa32278fd6dd0aea9f48f50833e
KAMaz64 <konstantin.a.mazunin@gmail.com>
2023-02-21 01:13:20 +0300

    completed task 2nd version

--- scratches/Program.cs
+++ scratches/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/* 
 Console.Clear();
 
 Console.WriteLine("Введите элементы нового массива через пробел");
@@ -20,4 +20,45 @@ void PrintArrayOfShortElements(string[] array)
     }
     Console.WriteLine();
 }
+ */
+Console.Clear();
 
+Console.WriteLine("Введите элементы нового массива через пробел");
+
+string[] newArray = Console.ReadLine().Split(" ");
+Console.WriteLine();
+
+int NumberOfShort = 0;
+for (int i = 0; i < newArray.Length; i++)
+{
+    if (newArray[i].Length <= 3)
+    {
+        NumberOfShort++;
+    }
+}
+
+int x = NumberOfShort;
+
+string[] newArrayShort = new string[x]; 
+int j = 0;
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    if (newArray[i].Length <= 3)
+    {
+        newArrayShort[j] = newArray[i];
+        j++;
+    }
+}
+
+Console.WriteLine("Массив из элементов заданного массива, длина которых не превышает 3:");
+PrintArray(newArrayShort);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
